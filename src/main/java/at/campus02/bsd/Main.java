0ec5279e8a3e91f4f72373d1bde45b695c2356a2
Samuel Ulz <s54090@edu.campus02.at>
2025-04-17 17:15:02 +0200

    updated my calculator project

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,14 +1,39 @@
 package at.campus02.bsd;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double number1 = Double.parseDouble(args[0]);
-        double number2 = Double.parseDouble(args[1]);
+        Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
+        double number1, number2;
+
+        System.out.println("Samuel Ulz\n");
+
+        number1 = userInput(scanner, true);
+        number2 = userInput(scanner, false);
 
         System.out.println(number1 + " + " +  number2 + " = " + calc.add(number1, number2));
         System.out.println(number1 + " - " +  number2 + " = " + calc.subtract(number1, number2));
         System.out.println(number1 + " / " +  number2 + " = " + calc.divide(number1, number2));
         System.out.println(number1 + " * " +  number2 + " = " + calc.multiply(number1, number2));
+
+        scanner.close();
+    }
+
+    public static double userInput(Scanner scanner, boolean first) {
+        double value = 0;
+
+        do {
+            System.out.print((first) ? "Enter first number: " : "Enter second number: ");
+            if (scanner.hasNextDouble()) {
+                value = scanner.nextDouble();
+                scanner.nextLine();
+                return value;
+            }
+            System.out.println("Please enter a valid number!");
+            scanner.nextLine();
+        } while (true);
     }
 }
+
